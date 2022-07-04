1cd82efd5298f4e9edb4c3f39a9f8ef6112813e0
jhuril45 <jhuril45@gmail.com>
2022-07-04 09:58:27 +0800

    Updates

--- wp-content/themes/pio-theme/template-parts/content-footer.php
+++ wp-content/themes/pio-theme/template-parts/content-footer.php
@@ -132,11 +132,11 @@
       </q-list>
     </div>
   </div>
-  <q-toolbar class="bg-blue-10 text-white q-px-lg" v-if="$q.screen.gt.sm">
+  <q-toolbar class="bg-blue-10 text-white q-px-lg relative-position" v-if="$q.screen.gt.sm">
     <q-toolbar-title class="text-body2 text-italic text-center">
       Ⓒ 2022. City Government of Butuan. All Rights Reserved.
     </q-toolbar-title>
-    <span class="text-body2">
+    <span class="text-body2 absolute-right row items-center q-px-sm">
       Version 1.0.0
     </span>
   </q-toolbar>
